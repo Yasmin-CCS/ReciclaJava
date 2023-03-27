fc499b8bcb3f11ce5cf25817c15a448cafb9c28f
Yasmin <yas.carozzi@gmail.com>
2023-03-27 20:02:52 -0300

    Alterei o Controller, colocando as msgs de doação e as opções caso o cliente não queira cadastrar o CPF. Acrescentei um package com a classe doação que possui as mensagens de doação. Quando forem criar o menu com 3 opções, podem só linkar essa classe e puxar as msgs daí

--- reciclarJava/src/reciclarJa/controller/ReciclarJaController.java
+++ reciclarJava/src/reciclarJa/controller/ReciclarJaController.java
@@ -2,24 +2,52 @@ package reciclarJa.controller;
 
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import reciclarJa.doacao.*;
 import reciclarJa.model.Pessoa;
 import reciclarJa.repository.ReciclarJaRepository;
 
+
 public class ReciclarJaController implements ReciclarJaRepository {
+	
+	Scanner leia = new Scanner (System.in);
+	campodoacao doacao =  new campodoacao();
 	
 	
 	private ArrayList<Pessoa> listaClientes = new ArrayList<Pessoa>();
-
+	int opcao;
+	
 	@Override// Localizar o CPF na ArrayList listaClientes
 	public void procurarPorCpf(String cpf) {
 		var pessoa = buscarNaCollection(cpf);
 		
-		if(pessoa != null)
+		if(pessoa != null) {
 			pessoa.visualizar();
-		else
-			System.out.println("\n O CPF : "+cpf+" não possui cadastro! ");
+		//colocar aqui o uma mensagem de agradecimento por ter salvo os créditos
+		} else {
+			System.out.println("============================================================================================");
+			System.out.println("||                                                                                        ||");                                                                                    
+			System.out.println("||                            O CPF não possui cadastro!                                  ||"); 
+			System.out.println("||           Para creditar, precisamos cadastrar seu CPF, deseja continuar?               ||");
+			System.out.println("||                               (1) Sim    |    (2) Não                                  ||");
+			System.out.println("||                                                                                        ||");
+			System.out.println("============================================================================================");
+			//lendo a opção
+			opcao = leia.nextInt ();
+		}
 		
+		switch (opcao) {
+		//caso ele queira cadastrar: linkar aqui a parte de cadastro:
+		case 1:
+			break;
+		//caso ele diga que não quer cadastrar:
+		case 2:
+		//vai aparecer a mensagem avisando que os valores vão ser doados e vai ler a resposta
+			doacao.msgdoacao();
+		//se a resposta for positiva ele vai doar e aparecer a mensagem de agradecimento
+			doacao.doar(opcao);
+		}
 	}
 
 	@Override // Método para puxar o cadastro
@@ -38,14 +66,14 @@ public class ReciclarJaController implements ReciclarJaRepository {
 	}
 
 	// Buscar cliente na ArrayList ListaClientes
-	public Pessoa buscarNaCollection(String cpf) {
+	public Pessoa buscarNaCollection (String cpf) {
 		for (var pessoa:listaClientes) {
 			if (pessoa.getCpf() == cpf) {
-				return pessoa;	
-			}
+				return pessoa;		
+			}		
 		}
 		return null;
-		
-	}
-
+	 }
+	
 }
+	
